62510bfc12d7be702161be2060dc0afa877d9fe7
Jeff Connelly <shellreef@gmail.com>
2010-11-04 21:46:41 -0700

    process_image: add image saving, but no processing yet

--- process_images.py
+++ process_images.py
@@ -8,34 +8,58 @@ import pprint
 import os
 import Image
 
-files = os.listdir(".")
-byColor = {}
-byName = {}
-for filename in files:
-    if filename.startswith("gr"):
-        prefix = "gr"
-        rest = filename[2:]
-    elif filename.startswith("lb"):
-        prefix = "lb"
-        rest = filename[2:]
+def main():
+    names = availablePieces()
+    print "Loaded %s pieces" % (len(names),)
+    for name in names:
+        bImage = Image.open("b" + name + ".gif")
+        wImage = Image.open("w" + name + ".gif")
+
+        saveImage(bImage, "/tmp/a" + name + ".gif")
+
+def saveImage(image, filename):
+    if image.info.has_key("transparency"):
+        # PIL Image has no concept of transparency, lame
+        # Workaround from http://www.velocityreviews.com/forums/t354000-pil-and-transparent-gifs.html
+        kwargs = {"transparency": image.info["transparency"]}
     else:
-        prefix = filename[0]
-        rest = filename[1:]
+        kwargs = {}
+    image.save(filename, **kwargs)
 
-    if "." not in rest:
-        continue
-    name, suffix = rest.rsplit(".", 1)
-    if suffix != "gif":
-        continue
+def availablePieces():
+    # TODO: Instead of all this directory junk, read an index (issue #6)
+    files = os.listdir(".")
+    byColor = {}
+    byName = {}
+    for filename in files:
+        if filename.startswith("gr"):
+            prefix = "gr"
+            rest = filename[2:]
+        elif filename.startswith("lb"):
+            prefix = "lb"
+            rest = filename[2:]
+        else:
+            prefix = filename[0]
+            rest = filename[1:]
 
-    if not byColor.has_key(prefix):
-        byColor[prefix] = {}
-    if not byName.has_key(name):
-        byName[name] = []
+        if "." not in rest:
+            continue
+        name, suffix = rest.rsplit(".", 1)
+        if suffix != "gif":
+            continue
 
-    byColor[prefix][name] = True
-    byName[name].append(prefix)
+        if not byColor.has_key(prefix):
+            byColor[prefix] = {}
+        if not byName.has_key(name):
+            byName[name] = []
 
-print pprint.pprint(byName)
+        byColor[prefix][name] = True
+        byName[name].append(prefix)
 
-print [(name, colors) for name, colors in byName.iteritems() if len(colors) < 2]
+    #print [(name, colors) for name, colors in byName.iteritems() if len(colors) < 2]
+
+    # Available pieces
+    return [name for name, colors in byName.iteritems() if "b" in colors and "w" in colors]
+
+if __name__ == "__main__":
+    main()
