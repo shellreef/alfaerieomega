7f779d40c4779340cf4fd247ac3525db7d360338
Jeff Connelly <shellreef@gmail.com>
2010-11-06 00:39:27 -0700

    Integrate Alfaerie Expansion Set 6

--- process_images.py
+++ process_images.py
@@ -135,7 +135,7 @@ def updatePieceIndex():
 
         if not index.has_key(name):
             print "New piece:", name
-            index[name] = {"set": ["Alfaerie Expansion Set 6"]}   #findImageSet("w"+name+".gif")}
+            #index[name] = {"set": ["Alfaerie Expansion Set 6"]}   #findImageSet("w"+name+".gif")}
         # Otherwise, leave existing entry (preserve metadata)
 
     # TODO: more info (credits, don't overwrite but reconcile existing), make the list a dict
