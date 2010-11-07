09bfaef5f5bfd2de00f3a1bb4607058ff5c10e10
Jeff Connelly <shellreef@gmail.com>
2010-11-06 22:36:43 -0700

    JSON index: make 'set' a scalar not a list

--- cleanup.py
+++ cleanup.py
@@ -3,12 +3,6 @@
 import json
 d = json.loads(file("index.json").read())
 for k in d:
-    if "Alfaerie Expansion Set 4" in d[k]["set"]:
-        d[k]["set"] = ["Alfaerie Expansion Set 4"]
-    if "Alfaerie" in d[k]["set"]:
-        d[k]["set"] = ["Alfaerie"]
+    d[k]["set"] = d[k]["set"][0]
 
-
-    if len(d[k]["set"]) != 1:
-        print k, d[k]
-
+file("index.json","w").write(json.dumps(d))
