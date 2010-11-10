479e4bba10e5a00fbd076196fca001e61ea94937
Jeff Connelly <shellreef@gmail.com>
2010-11-09 23:10:41 -0800

    Index: Add longname

--- cleanup.py
+++ cleanup.py
@@ -3,8 +3,6 @@
 import json
 d = json.loads(file("index.json").read())
 for k in d:
-    tags = d[k].get("tags", [])
-    if len(tags) == 0:
-        del d[k]["tags"] 
+    d[k]["longname"] = k[0].upper() + k[1:]
 
 file("index.json","w").write(json.dumps(d))
