309549e373a26fdc149b968f41a504ff499803cf
Jeff Connelly <shellreef@gmail.com>
2010-11-11 22:12:51 -0800

    Add names to digits

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,17 @@
 # Cleanup index.json
 #
 import json
+import re
 d = json.loads(file("index.json").read())
 for k in d:
-    d[k]["longname"] = k[0].upper() + k[1:]
+    tags = d[k].get("tags", [])
+
+    print k
+    if re.search(r'-[A-Z]', d[k]["longname"]):
+        print "*",d[k]["longname"]
+        tags.append("compound")
+
+    if len(tags) != 0:
+        d[k]["tags"] = tags
 
 file("index.json","w").write(json.dumps(d))
